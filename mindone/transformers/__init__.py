834f00ee6f3e6b4ca8610c1b7dc99f98c0e2c1a7
fighting-ye <1138455646@qq.com>
2025-05-21 16:09:43 +0800

    add modernbert

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -192,6 +192,14 @@ from .models.whisper import (
     WhisperPreTrainedModel,
     WhisperProcessor,
 )
+from .models.modernbert import (
+    ModernBertModel,
+    ModernBertForMaskedLM,
+    ModernBertForQuestionAnswering,
+    ModernBertForSequenceClassification,
+    ModernBertForTokenClassification,
+    ModernBertPreTrainedModel,
+)
 from .models.xlm_roberta import XLMRobertaModel, XLMRobertaPreTrainedModel
 from .pipelines import TextGenerationPipeline, pipeline
 from .processing_utils import ProcessorMixin
