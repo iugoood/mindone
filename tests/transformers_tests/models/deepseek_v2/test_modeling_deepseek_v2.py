b0cb595babe3c992a11d3623a865baf8644c4505
fighting-ye <1138455646@qq.com>
2025-08-06 15:47:38 +0800

    add deepseek_v2 & deepseek_v3 models

---
+++ tests/transformers_tests/models/deepseek_v2/test_modeling_deepseek_v2.py
@@ -0,0 +1,159 @@
+# coding=utf-8
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Testing suite for the Mindspore DeepSeekV2 model."""
+import inspect
+
+import numpy as np
+import pytest
+import torch
+from transformers.models.deepseek_v2.modeling_deepseek_v2 import DeepseekV2Model,DeepseekV2ForCausalLM,DeepseekV2ForSequenceClassification
+from transformers.models.deepseek_v2.configuration_deepseek_v2 import DeepseekV2Config
+
+import mindspore as ms
+
+from tests.modeling_test_utils import (
+    MS_DTYPE_MAPPING,
+    PT_DTYPE_MAPPING,
+    compute_diffs,
+    generalized_parse_args,
+    get_modules,
+)
+from ...causal_lm_tester import CausalLMModelTester
+
+# CrossEntropyLoss not support bf16
+DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3}
+MODES = [1]
+
+class DeepseekV2ModelTester(CausalLMModelTester):
+    config_class = DeepseekV2Config
+    base_model_class = DeepseekV2Model
+    causal_lm_class = DeepseekV2ForCausalLM
+    sequence_class = DeepseekV2ForSequenceClassification
+
+    def __init__(
+        self,
+        n_routed_experts=8,
+        kv_lora_rank=32,
+        q_lora_rank=16,
+        qk_nope_head_dim=64,
+        qk_rope_head_dim=64,
+    ):
+        super().__init__()
+        self.n_routed_experts = n_routed_experts
+        self.kv_lora_rank = kv_lora_rank
+        self.q_lora_rank = q_lora_rank
+        self.qk_nope_head_dim = qk_nope_head_dim
+        self.qk_rope_head_dim = qk_rope_head_dim
+
+model_tester = DeepseekV2ModelTester()
+(
+    config,
+    inputs_dict,
+) = model_tester.prepare_config_and_inputs_for_common()
+
+
+DEEPSEEKV2_CASES = [
+    [
+        "DeepseekV2Model",
+        "transformers.DeepseekV2Model",
+        "mindone.transformers.DeepseekV2Model",
+        (config,),
+        {},
+        (),
+        {
+            "inputs_id": inputs_dict["inputs_id"],
+            "attention_mask": inputs_dict["attention_mask"],
+        },
+        {
+            "last_hidden_state": 0,
+        },
+    ]
+]
+
+
+@pytest.mark.parametrize(
+    "name,pt_module,ms_module,init_args,init_kwargs,inputs_args,inputs_kwargs,outputs_map,dtype,mode",
+    [
+        case
+        + [
+            dtype,
+        ]
+        + [
+            mode,
+        ]
+        for case in DEEPSEEKV2_CASES
+        for dtype in DTYPE_AND_THRESHOLDS.keys()
+        for mode in MODES
+    ],
+)
+def test_named_modules(
+    name,
+    pt_module,
+    ms_module,
+    init_args,
+    init_kwargs,
+    inputs_args,
+    inputs_kwargs,
+    outputs_map,
+    dtype,
+    mode,
+):
+    ms.set_context(mode=mode)
+
+    (
+        pt_model,
+        ms_model,
+        pt_dtype,
+        ms_dtype,
+    ) = get_modules(pt_module, ms_module, dtype, *init_args, **init_kwargs)
+    pt_inputs_args, pt_inputs_kwargs, ms_inputs_args, ms_inputs_kwargs = generalized_parse_args(
+        pt_dtype, ms_dtype, *inputs_args, **inputs_kwargs
+    )
+
+    # set `hidden_dtype` if requiring, for some modules always compute in float
+    # precision and require specific `hidden_dtype` to cast before return
+    if "hidden_dtype" in inspect.signature(pt_model.forward).parameters:
+        pt_inputs_kwargs.update({"hidden_dtype": PT_DTYPE_MAPPING[pt_dtype]})
+        ms_inputs_kwargs.update({"hidden_dtype": MS_DTYPE_MAPPING[ms_dtype]})
+    ms_inputs_kwargs["return_dict"] = False
+
+    with torch.no_grad():
+        pt_outputs = pt_model(*pt_inputs_args, **pt_inputs_kwargs)
+    ms_outputs = ms_model(*ms_inputs_args, **ms_inputs_kwargs)
+    # print("ms:", ms_outputs)
+    # print("pt:", pt_outputs)
+    if outputs_map:
+        pt_outputs_n = []
+        ms_outputs_n = []
+        for pt_key, ms_idx in outputs_map.items():
+            # print("===map", pt_key, ms_idx)
+            pt_output = getattr(pt_outputs, pt_key)
+            ms_output = ms_outputs[ms_idx]
+            if isinstance(pt_output, (list, tuple)):
+                pt_outputs_n += list(pt_output)
+                ms_outputs_n += list(ms_output)
+            else:
+                pt_outputs_n.append(pt_output)
+                ms_outputs_n.append(ms_output)
+        diffs = compute_diffs(pt_outputs_n, ms_outputs_n)
+    else:
+        diffs = compute_diffs(pt_outputs, ms_outputs)
+
+    THRESHOLD = DTYPE_AND_THRESHOLDS[ms_dtype]
+    assert (np.array(diffs) < THRESHOLD).all(), (
+        f"ms_dtype: {ms_dtype}, pt_type:{pt_dtype}, "
+        f"Outputs({np.array(diffs).tolist()}) has diff bigger than {THRESHOLD}"
+    )
+
